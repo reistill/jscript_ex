ed7fb7ec2bdba2d72258767d54e586b83295d92c
reistill <joelreistill@gmail.com>
2025-01-24 16:35:48 -0300

    testes_vetor

---
+++ aula16/vetor.js
@@ -0,0 +1,14 @@
+let num = [5, 8, 9, 1, 3, 7, 2]
+
+num[6] = 6
+console.log(num)
+
+num.push(0)
+console.log(num)
+
+console.log(`O primiero valor do vetor num é: ${num[0]}`)
+
+//tamanho do array num
+console.log(num.length)
+
+console.log(`Esse é o vetor num, só que organizado: ${num.sort()}`)
