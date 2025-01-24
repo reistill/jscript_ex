b815be68436806eea23d2e2b529929ed55014a40
reistill <joelreistill@gmail.com>
2025-01-24 17:35:01 -0300

    ex_vetores

--- aula16/vetor.js
+++ aula16/vetor.js
@@ -1,4 +1,5 @@
 let num = [5, 8, 9, 1, 3, 7, 2]
+console.log(num)
 
 num[6] = 6
 console.log(num)
@@ -6,9 +7,30 @@ console.log(num)
 num.push(0)
 console.log(num)
 
-console.log(`O primiero valor do vetor num é: ${num[0]}`)
+console.log(`O primeiro valor do vetor num é: ${num[0]} \n`)
 
 //tamanho do array num
-console.log(num.length)
+console.log(`Esse é o tamanho do vetor num: ${num.length} \n`)
 
-console.log(`Esse é o vetor num, só que organizado: ${num.sort()}`)
+//procurar o valor 7 no array num
+console.log(`Esssa é a posição do valor 7 no vetor num: ${num.indexOf(7)} \n`)
+
+//console.log(`Esse é o vetor num, só que organizado: ${num.sort()} \n`)
+
+//for padrão para percorer um array
+for (pos = 0; pos < num.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]} \n`)
+}
+
+//for simplificado para percorrer um array
+for (pos in num) {
+    console.log(`O valor ${num[pos]} está na posição ${pos}`)
+}
+
+//caso o indexOf() não encontre o valor escolhido, ele retorna -1
+let posi = num.indexOf(3)
+if (posi == -1) {
+    console.log(`O valor ${posi} não foi encontrado`)
+} else {
+    console.log(`O valor ${posi} foi encontrado na posição ${posi}`)
+}
